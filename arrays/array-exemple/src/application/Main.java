db14dca2ffdfef2cb6f7d1e71add42e34e75d8f2
mlobo-dev <mlobo.dev@gmail.com>
2019-10-14 21:51:10 -0300

    Basic Array exemple

---
+++ arrays/array-exemple/src/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// array declaration
+		int[] intArray = { 10, 20, 30, 40, 50 };
+
+		System.out.println("Array on position intArray[1] = " + intArray[1]);
+		System.out.println("Total elements" + intArray.length);
+	}
+
+}
