517c7a6255a487f35fd4779704a0a1c409ae11d9
mlobo-dev <mlobo.dev@gmail.com>
2019-10-15 18:50:34 -0300

    Foreach over an Array

---
+++ conditionals and loops/foreach-over-array/src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char[] array = { 'a', 'e', 'i', 'o', 'u' };
+
+		for (char c : array) {
+			System.out.println(c);
+		}
+	}
+
+}
