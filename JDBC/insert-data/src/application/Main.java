9998d09b8a5c55ee1dc66519a23af3f80c0aeace
mlobo-dev <mlobo.dev@gmail.com>
2019-10-16 21:43:10 -0300

    Recovering de generated ID

--- JDBC/insert-data/src/application/Main.java
+++ JDBC/insert-data/src/application/Main.java
@@ -3,13 +3,13 @@ package application;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.ConnectionFactory;
-
-
 
 public class Main {
 
@@ -17,32 +17,40 @@ public class Main {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection con = null;
 		PreparedStatement stm = null;
-		
+
 		try {
 			con = ConnectionFactory.getConnection();
-			stm = con.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+"VALUES "
-					+"(?,?,?,?,?)");
-			
-			stm.setString(1, "Carl Purple");
-			stm.setString(2, "carl@gmail.com");
-			stm.setDate(3, new Date(sdf.parse("10/16/2019").getTime()) );
+			stm = con.prepareStatement(
+					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)" + "VALUES " + "(?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			stm.setString(1, "joh Purple");
+			stm.setString(2, "joh@gmail.com");
+			stm.setDate(3, new Date(sdf.parse("10/16/2019").getTime()));
 			stm.setDouble(4, 3000.0);
 			stm.setInt(5, 4);
-			
-			int rowAffected = stm.executeUpdate();
-			System.out.println("done! rows affected: " + rowAffected);
-		}
-		catch(SQLException e) {
+
+			int rowsAffected = stm.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = stm.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("ID: " + id );
+				}
+				
+			} else {
+				System.out.println("no rows affected");
+			}
+
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		catch(ParseException e) {
+		} catch (ParseException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			ConnectionFactory.closeConnection(con, stm);
 		}
-		
+
 	}
 
 }
