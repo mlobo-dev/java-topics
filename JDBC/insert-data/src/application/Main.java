69cd33e70b57b9c7943385e254fad950cf249bd1
mlobo-dev <mlobo.dev@gmail.com>
2019-10-16 22:33:32 -0300

    executing an update on database

--- JDBC/insert-data/src/application/Main.java
+++ JDBC/insert-data/src/application/Main.java
@@ -1,55 +1,33 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.ConnectionFactory;
 
 public class Main {
 
 	public static void main(String[] args) {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Connection con = null;
-		PreparedStatement stm = null;
-
-		try {
-			con = ConnectionFactory.getConnection();
-			stm = con.prepareStatement(
-					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)" + "VALUES " + "(?,?,?,?,?)",
-					Statement.RETURN_GENERATED_KEYS);
-
-			stm.setString(1, "joh Purple");
-			stm.setString(2, "joh@gmail.com");
-			stm.setDate(3, new Date(sdf.parse("10/16/2019").getTime()));
-			stm.setDouble(4, 3000.0);
-			stm.setInt(5, 4);
-
-			int rowsAffected = stm.executeUpdate();
-
-			if (rowsAffected > 0) {
-				ResultSet rs = stm.getGeneratedKeys();
-				while(rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("ID: " + id );
-				}
+			Connection con = null;
+			PreparedStatement st = null;
+			
+			try {
+				con = ConnectionFactory.getConnection();
 				
-			} else {
-				System.out.println("no rows affected");
+				st = con.prepareStatement(
+				"UPDATE seller SET BaseSalary = BaseSalary + ? WHERE (DepartmentID = ?)");
+						
+				st.setDouble(1, 200.0);
+				st.setInt(2, 2);
+				
+				int rowsAffected = st.executeUpdate();
+				
+				System.out.println("Done! rows Affected: " + rowsAffected);
+				
+			}catch(SQLException e) {
+				e.printStackTrace();
 			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		} finally {
-			ConnectionFactory.closeConnection(con, stm);
-		}
 
 	}
 
