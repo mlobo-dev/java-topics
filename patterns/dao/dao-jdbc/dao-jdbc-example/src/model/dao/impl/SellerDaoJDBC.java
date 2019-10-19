d07c82158c169175f1dcf7317751d97592823520
mlobo-dev <mlobo.dev@gmail.com>
2019-10-19 15:18:49 -0300

    Reusing instantiation

--- patterns/dao/dao-jdbc/dao-jdbc-example/src/model/dao/impl/SellerDaoJDBC.java
+++ patterns/dao/dao-jdbc/dao-jdbc-example/src/model/dao/impl/SellerDaoJDBC.java
@@ -34,8 +34,6 @@ public class SellerDaoJDBC implements SellerDao {
 	public void deleteById(Integer id) {
 
 	}
-	
-	
 
 	@Override
 	public Seller findById(Integer id) {
@@ -50,26 +48,36 @@ public class SellerDaoJDBC implements SellerDao {
 			stm.setInt(1, id);
 			rs = stm.executeQuery();
 			if (rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-
-				Seller seller = new Seller();
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				seller.setBirthDate(rs.getDate("BirthDate"));
-				seller.setDepartment(dep);
+				Department dep = instantiateDepartment(rs);
+				Seller seller = instantiateSeller(rs, dep);
 				return seller;
 			}
 			return null;
-		
+
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
-			ConnectionFactory.closeConnection(con, stm, rs);;
+			ConnectionFactory.closeConnection(con, stm, rs);
+			;
 		}
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setDepartment(dep);
+		return seller;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
