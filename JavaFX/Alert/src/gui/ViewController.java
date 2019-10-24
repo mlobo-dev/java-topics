cfdb98e3fa35ab79fc12ae679bdf5deea9ed4efa
mlobo-dev <mlobo.dev@gmail.com>
2019-10-23 21:04:47 -0300

    JavaFX Alerts

---
+++ JavaFX/Alert/src/gui/ViewController.java
@@ -0,0 +1,18 @@
+package gui;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
+
+public class ViewController {
+
+	@FXML
+	private Button btTeste;
+
+	@FXML
+	public void onBtTestAction() {
+		Alerts.showAlert("Alert Title", "Alert Header", "Hello", AlertType.WARNING);
+
+	}
+}
